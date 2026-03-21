5ea9a68a9b57d1057aa8b65ec47e79f872e92c76
Semenova Veronika <11sssvvv11@gmail.com>
2026-03-21 09:51:00 +0300

    feat: add routing and FSD structure (#13)

---
+++ src/app/router/index.tsx
@@ -0,0 +1,65 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { routePaths } from "@/shared/config/routePaths";
+
+import { LoginPage } from "@/pages/auth/login";
+import { RegisterPage } from "@/pages/auth/register";
+import { ProjectsPage } from "@/pages/projects/projects-list";
+import { ProjectDetailsPage } from "@/pages/projects/project-details";
+import { EditorPage } from "@/pages/editor/diagram-editor";
+import { ProfilePage } from "@/pages/profile";
+
+import { PrivateRoute } from "./PrivateRoute";
+
+export const router = createBrowserRouter([
+  {
+    path: routePaths.login,
+    element: <LoginPage />,
+  },
+  {
+    path: routePaths.register,
+    element: <RegisterPage />,
+  },
+
+  // private
+  {
+    path: routePaths.home,
+    element: (
+      <PrivateRoute>
+        <ProjectsPage />
+      </PrivateRoute>
+    ),
+  },
+  {
+  path: routePaths.projects,
+  element: (
+    <PrivateRoute>
+      <ProjectsPage />
+    </PrivateRoute>
+  ),
+  },
+  {
+    path: routePaths.project,
+    element: (
+      <PrivateRoute>
+        <ProjectDetailsPage />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: routePaths.editor,
+    element: (
+      <PrivateRoute>
+        <EditorPage />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: routePaths.profile,
+    element: (
+      <PrivateRoute>
+        <ProfilePage />
+      </PrivateRoute>
+    ),
+  },
+]);
---
+++ src/pages/projects/projects-list/ui/ProjectsPage.tsx
@@ -0,0 +1,10 @@
+const ProjectsPage = () => {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <p>Список проектов</p>
+    </div>
+  );
+};
+
+export default ProjectsPage;
