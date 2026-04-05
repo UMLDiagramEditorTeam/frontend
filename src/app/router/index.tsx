4896a4d112e9a48001ee826889f179c9ae4ef5f3
Vsevolod <si34284@gmail.com>
2026-04-05 11:53:29 +0300

    feat: верстка страниц авторизации и списка проектов (#16)
    * add fsd routing
    
    * Сверстал следующие страницы: стартовую, входа, регистрации, страницу управления проектами (надо будет доделать потом еще)
    
    * fixed the comments on the review
    
    ---------
    
    Co-authored-by: avewrrrr <11sssvvv11@gmail.com>

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,15 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
-import { routePaths } from "@/shared/config/routePaths";
+import { routePaths } from '@/shared/config/routePaths';
 
-import { LoginPage } from "@/pages/auth/login";
-import { RegisterPage } from "@/pages/auth/register";
-import { ProjectsPage } from "@/pages/projects/projects-list";
-import { ProjectDetailsPage } from "@/pages/projects/project-details";
-import { EditorPage } from "@/pages/editor/diagram-editor";
-import { ProfilePage } from "@/pages/profile";
+import { MainPage } from '@/pages/main/ui/MainPage';
+import { LoginPage } from '@/pages/auth/login';
+import { RegisterPage } from '@/pages/auth/register';
+import { ProjectsPage } from '@/pages/projects/projects-list';
+import { ProjectDetailsPage } from '@/pages/projects/project-details';
+import { EditorPage } from '@/pages/editor/diagram-editor';
+import { ProfilePage } from '@/pages/profile';
 
-import { PrivateRoute } from "./PrivateRoute";
+import { PrivateRoute } from './PrivateRoute';
 
 export const router = createBrowserRouter([
   {
@@ -23,20 +24,16 @@ export const router = createBrowserRouter([
 
   // private
   {
-    path: routePaths.home,
+    path: '/',
+    element: <MainPage />,
+  },
+  {
+    path: routePaths.projects,
     element: (
       <PrivateRoute>
         <ProjectsPage />
       </PrivateRoute>
     ),
-  },
-  {
-  path: routePaths.projects,
-  element: (
-    <PrivateRoute>
-      <ProjectsPage />
-    </PrivateRoute>
-  ),
   },
   {
     path: routePaths.project,
