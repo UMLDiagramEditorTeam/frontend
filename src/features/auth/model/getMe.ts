58122d64071cd8aa60aec8685a98d85093d8fc6c
avewrrrr <11sssvvv11@gmail.com>
2026-04-23 14:52:43 +0300

    fix all model files

--- src/features/auth/model/getMe.ts
+++ src/features/auth/model/getMe.ts
@@ -1,10 +1,12 @@
 import { authApi } from '../api/auth.api';
-import { setUser } from './user.store';
+import type { User } from '@/shared/api/generated/data-contracts';
 
-export const getMe = async () => {
+export const getMe = async (): Promise<User> => {
   const resp = await authApi.getAuth();
-  if (resp.data) {
-    setUser(resp.data);
+
+  if (!resp.data) {
+    throw new Error('Failed to fetch user');
   }
+
   return resp.data;
 };
