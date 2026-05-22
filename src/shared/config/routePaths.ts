d2465e60ae88bb62897af1b0748747ba3c8dcadc
Semenova Veronika <11sssvvv11@gmail.com>
2026-05-22 15:16:17 +0300

    update routePaths.ts

--- src/shared/config/routePaths.ts
+++ src/shared/config/routePaths.ts
@@ -4,13 +4,18 @@ export const routePaths = {
   register: '/register',
 
   projects: '/projects',
+  profile: '/profile',
+
   editor: '/projects/:projectId',
+  codeUpload: '/projects/:projectId/import-code',
+  export: '/projects/:projectId/export',
+  codeGeneration: '/projects/:projectId/generate-code',
 
   projectEditor: (projectId: string | number) => `/projects/${projectId}`,
-
-  codeUpload: '/import-code',
-  export: '/export',
-  codeGeneration: '/generate-code',
-
-  profile: '/profile',
+  projectCodeUpload: (projectId: string | number) =>
+    `/projects/${projectId}/import-code`,
+  projectExport: (projectId: string | number) =>
+    `/projects/${projectId}/export`,
+  projectCodeGeneration: (projectId: string | number) =>
+    `/projects/${projectId}/generate-code`,
 };
